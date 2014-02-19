77f841738f06d9ccaa5e95f9f5e6d855e0337924
Evan Payne <jpayne0061@gmail.com>
2014-02-19 15:24:41 -0500

    trying again to fix attr_accessor error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,8 +3,10 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  attr_accessor :image_file_name
-  attr_accessor :image_content_type
-  attr_accessor :image_file_size
-  attr_accessor :image_updated_at
+  
+  #attr_accessible :image_file_name
+  #attr_accessor :image_file_name
+  #attr_accessor :image_content_type
+  #attr_accessor :image_file_size
+  #attr_accessor :image_updated_at
 end
