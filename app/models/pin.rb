ff31cfb710294b0a3bee63c9fafce92a6726f0af
Evan Payne <jpayne0061@gmail.com>
2014-02-19 13:56:55 -0500

    tryin to fix 'required attr' called by heroku logs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,7 @@
 class Pin < ActiveRecord::Base
 
   belongs_to :user
+  attr_accessible :image_file_name
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
