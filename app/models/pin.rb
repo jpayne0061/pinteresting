4a614b355edd79580bed12786bebb34a683154ee
Evan Payne <jpayne0061@gmail.com>
2014-02-19 23:10:41 -0500

    added jquery masonry

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
 
   belongs_to :user
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
   #attr_accessible :image_file_name
   #attr_accessor :image_file_name
