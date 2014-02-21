5813507c9867305cc07347a1e5090fdd074b289f
Evan Payne <jpayne0061@gmail.com>
2014-02-21 10:56:32 -0500

    added glyphicons and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
   get "mystical_cat_pics" => "pages#mystical_cat_pics"
   # The priority is based upon order of creation: first created -> highest priority.
